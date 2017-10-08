213feefa169ef1c23d65c4f3481f2750c0df77d8
Kris <kris.weber.1994@gmail.com>
2017-10-08 16:17:06 -0700

    Changed individual site pages to generic Page component

---
+++ src/layout/Page.js
@@ -0,0 +1,15 @@
+import React , { Component } from 'react';
+import PageBody from './PageBody';
+import './App.css';
+
+class Page extends Component {
+  render() {
+    return (
+      <div>
+        <PageBody />
+      </div>
+    )
+  }
+}
+
+export default Page;
