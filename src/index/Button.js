b6c09b0322dee153fee67352fa8f9ef0706781c8
Kris <kris.weber.1994@gmail.com>
2017-10-08 15:43:56 -0700

    Add skeletons to index components

--- src/index/Button.js
+++ src/index/Button.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+class Button extends Component {
+  render() {
+    return ()
+  }
+}
+
 export default Button;
--- src/index/ImageHolder.js
+++ src/index/ImageHolder.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+class ImageHolder extends Component {
+  render() {
+    return ()
+  }
+}
+
 export default ImageHolder;
