3d517f4a69c52321ec93b4168df944667ab38546
Kris <kris.weber.1994@gmail.com>
2017-10-08 15:55:56 -0700

    Basic implementation of navbar

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import NavBarItem from './NavBarItem';
 
 class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      linkData: [
+        { text: 'Home', url: 'idk' },
+        { text: 'About', url: 'idk' },
+        { text: 'Curriculum', url: 'idk' },
+        { text: 'Contact', url: 'idk' },
+      ],
+    };
+  }
   render() {
-    return ()
+    const links = this.state.linkData.map(link =>
+    <NavBarItem text={link.text} url={link.url} />,
+  );
+    return (
+      <nav className="nav-main">
+        {links}
+      </nav>
+    )
   }
 }
 
