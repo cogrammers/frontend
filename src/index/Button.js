cd2aad9e0e61479fa3d50a3d7b03d3912ddc1e5e
Kris <kris.weber.1994@gmail.com>
2017-10-08 15:39:07 -0700

    Create folders to better organize components; add skeleton to component files

---
+++ src/index/Button.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default Button;
---
+++ src/index/TextBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TextBox extends Component {
+  render() {
+    return (
+      <div className="text-box">
+      </div>
+    )
+  }
+}
+
+export default TextBox;
