74001817ef613dd68d6e2d14bec521a387b6b120
Kris <kris.weber.1994@gmail.com>
2017-10-08 16:06:52 -0700

    Build out navbaritem component

--- src/layout/NavBarItem.js
+++ src/layout/NavBarItem.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import NavItem from 'react-bootstrap/lib/NavItem';
 import PropTypes from 'prop-types';
+import '../App.css';
 
-class NavBarItem extends Component {
-  render() {
-    return ()
-  }
+function NavBarItem(props) {
+  return (
+    <NavItem href={props.url}>{props.text}</NavItem>
+  );
 }
+
+NavBarItem.propTypes = {
+  text: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
 
 export default NavBarItem;
