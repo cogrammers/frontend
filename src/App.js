cbfe4044c8fbe8a709e2047958d3974f8063773e
Kris <kris.weber.1994@gmail.com>
2017-12-01 10:19:43 -0800

    Add boilerplate for react css grid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Grid from 'react-css-grid';
 import './App.css';
 import Content from './components/Content';
 import Mission from './components/Mission';
@@ -11,23 +12,34 @@ class App extends Component {
     }
   }
   render() {
-    let innerContent;
-    if (this.state.currentPage === 'mission') {
-      innerContent = <Mission />;
-    }
-    else {
-
-    }
     return (
-      <div>
-        <div className="app-header">
-          Cogrammers ...
-        </div>
-        <Content>
-          {innerContent}
-        </Content>
-      </div>
-    );
+      <Grid
+        width={320}
+        gap={24}>
+        <div>Column</div>
+        <div>Column</div>
+        <div>Column</div>
+        <div>Column</div>
+      </Grid>
+    )
+
+    // let innerContent;
+    // if (this.state.currentPage === 'mission') {
+    //   innerContent = <Mission />;
+    // }
+    // else {
+    //
+    // }
+    // return (
+    //   <div>
+    //     <div className="app-header">
+    //       Cogrammers ...
+    //     </div>
+    //     <Content>
+    //       {innerContent}
+    //     </Content>
+    //   </div>
+    // );
   }
 }
 
