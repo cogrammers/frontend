ca1f014a0fddcaf43bf01059d90e73d6ced4c821
Kala Wetzler <kala.wetzler@gmail.com>
2018-01-02 20:24:25 -0800

    Add contact page

---
+++ src/components/ContactPage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+class ContactPage extends Component{
+  constructor(){
+    super()
+    this.state = {
+      currentPage: 'contactPage'
+    }
+  }
+  render(){
+    return(
+      <div id="contact">
+        <p><a href="mailto:us@cogrammers.community">Email</a></p>
+        <p><a href="https://www.linkedin.com/company/cogrammers">LinkedIn</a></p>
+        <p><a href="https://www.meetup.com/cogrammers">Meetup</a></p>
+        <p><a href="https://www.facebook.com/cogrammers">Facebook</a></p>
+        <p><a href="https://www.twitter.com/cogrammers">Twitter</a></p>
+      </div>
+    )
+  }
+
+}
+
+export default ContactPage;
