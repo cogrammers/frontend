012feaf3af749c5df477ee0243867cb244332d1c
Anil Kulkarni <akulkarni@avvo.com>
2018-01-16 19:38:44 -0800

    fix jsxa11y error

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Menu extends Component {
   generateLink({text, index}) {
     const onClick = () => this.props.onClick(index);
-    return <li onClick={onClick}><a href="#" key={index}>{text}</a></li>;
+    return <li onClick={onClick}><a href={`#${text}`} key={index}>{text}</a></li>;
   }
   render() {
     const links = this.props.items.map(({text}, i) => this.generateLink({text, index: i}));
