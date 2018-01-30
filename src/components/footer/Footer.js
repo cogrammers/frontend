88f6f76bb057d0a5d6a4f33ee818b06dbe5f2a52
SEAjamieD <jamie.degnan@gmail.com>
2018-01-30 09:38:48 -0800

    added placeholders for future social logos to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,29 +1,24 @@
 import React, { Component } from 'react';
 import FooterBackground from '../../assets/footer-background.svg';
 import TagLogo from '../../assets/footer-tag-logo.svg';
+import SocialPlaceholder from '../../assets/icons/placeholder.svg';
 import './Footer.css';
 
 class Footer extends Component {
-  constructor(){
-    super()
-    this.state = {
-      currentPage: 'contactPage'
-    }
-  }
-
-
-  // <a href="mailto:info@cogrammers.community"><img className="contact-logo" src={MailIcon} alt="Email Icon"/></a>
-  // <a href="https://www.linkedin.com/company/cogrammers"><img className="contact-logo" src={LinkedInIcon} alt="LinkedIn Logo icon"/></a>
-  // <a href="https://www.meetup.com/cogrammers"><img className="contact-logo small" src={Meetup} alt="Meetup Logo icon"/></a>
-  // <a href="https://www.facebook.com/cogrammers"><img className="contact-logo" src={FacebookIcon} alt="Facebook Logo icon"/></a>
-  // <a href="https://www.twitter.com/cogrammers"><img className="contact-logo" src={TwitterIcon} alt="Twitter Logo icon"/></a>
-
 
   render(){
     return(
       <div className="footer">
         <img id="footer-background" alt="colorful background image" src={FooterBackground}/>
         <img id="header-logo-tagline" alt="Cogrammers logo and tagline" src={TagLogo}/>
+
+        <div className="social-container">
+          <a href="mailto:info@cogrammers.community"><img className="social-logo" src={SocialPlaceholder} alt="Email Icon"/></a>
+          <a href="https://www.linkedin.com/company/cogrammers"><img className="social-logo" src={SocialPlaceholder} alt="LinkedIn Logo icon"/></a>
+          <a href="https://www.meetup.com/cogrammers"><img className="social-logo" src={SocialPlaceholder} alt="Meetup Logo icon"/></a>
+          <a href="https://www.facebook.com/cogrammers"><img className="social-logo" src={SocialPlaceholder} alt="Facebook Logo icon"/></a>
+          <a href="https://www.twitter.com/cogrammers"><img className="social-logo" src={SocialPlaceholder} alt="Twitter Logo icon"/></a>
+        </div>
       </div>
     )
   }
