7ebfe089b71dc2e5b62bdabf34b09088856e1528
SEAjamieD <jamie.degnan@gmail.com>
2018-01-29 23:13:15 -0800

    Added footer coloring and tagline into footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
-import MailIcon from '../assets/icons/mail-icon.svg';
-import LinkedInIcon from '../assets/icons/linkedin-logo.svg';
-import Meetup from '../assets/icons/meetup-icon.svg';
-import FacebookIcon from '../assets/icons/facebook-icon.svg';
-import TwitterIcon from '../assets/icons/twitter-icon.svg';
-
+import FooterBackground from '../assets/footer-background.svg';
+import TagLogo from '../assets/footer-tag-logo.svg';
 import './Footer.css';
-
-
 
 class Footer extends Component {
   constructor(){
@@ -16,14 +10,20 @@ class Footer extends Component {
       currentPage: 'contactPage'
     }
   }
+
+
+  // <a href="mailto:info@cogrammers.community"><img className="contact-logo" src={MailIcon} alt="Email Icon"/></a>
+  // <a href="https://www.linkedin.com/company/cogrammers"><img className="contact-logo" src={LinkedInIcon} alt="LinkedIn Logo icon"/></a>
+  // <a href="https://www.meetup.com/cogrammers"><img className="contact-logo small" src={Meetup} alt="Meetup Logo icon"/></a>
+  // <a href="https://www.facebook.com/cogrammers"><img className="contact-logo" src={FacebookIcon} alt="Facebook Logo icon"/></a>
+  // <a href="https://www.twitter.com/cogrammers"><img className="contact-logo" src={TwitterIcon} alt="Twitter Logo icon"/></a>
+
+
   render(){
     return(
-      <div className="contact">
-        <a href="mailto:info@cogrammers.community"><img className="contact-logo" src={MailIcon} alt="Email Icon"/></a>
-        <a href="https://www.linkedin.com/company/cogrammers"><img className="contact-logo" src={LinkedInIcon} alt="LinkedIn Logo icon"/></a>
-        <a href="https://www.meetup.com/cogrammers"><img className="contact-logo small" src={Meetup} alt="Meetup Logo icon"/></a>
-        <a href="https://www.facebook.com/cogrammers"><img className="contact-logo" src={FacebookIcon} alt="Facebook Logo icon"/></a>
-        <a href="https://www.twitter.com/cogrammers"><img className="contact-logo" src={TwitterIcon} alt="Twitter Logo icon"/></a>
+      <div className="footer">
+        <img id="footer-background" alt="colorful background image" src={FooterBackground}/>
+        <img id="header-logo-tagline" alt="Cogrammers logo and tagline" src={TagLogo}/>
       </div>
     )
   }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,6 +14,7 @@ class Layout extends Component {
           <div>
             {this.props.children}
           </div>
+          <Footer/>
         </div>
 
     )
