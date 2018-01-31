0ca42567d24ccdb700dfef40c1cd374d1004b555
SEAjamieD <jamie.degnan@gmail.com>
2018-01-31 15:33:51 -0800

    Add a few more class options for simple formatting, move text to left and right according to design and shrink to 50% of container

--- src/components/body/pillars/Grow.js
+++ src/components/body/pillars/Grow.js
@@ -7,8 +7,8 @@ class Grow extends Component {
 
     return (
       <div className="pillar-container">
-        <h3 className="text-center">Community Building</h3>
-        <p>A core hypothesis of Cogrammers is that the program should directly benefit all those involved, as well as the larger community. Students get an education, alumni grow their network, companies hire better employees, the community gets apps made for them, and more.</p>
+        <h3 className="text-right">Community Building</h3>
+        <p className="width-50">A core hypothesis of Cogrammers is that the program should directly benefit all those involved, as well as the larger community. Students get an education, alumni grow their network, companies hire better employees, the community gets apps made for them, and more.</p>
         <p>The community aspect is incredibly important. Cogrammers is more than just a technical handbook. Everything we do is designed to help all of us grow as individuals. By learning empathy, communication, and teamworking skills, we become better people. We become better cohort mates, and better community members.</p>
       </div>
     )
--- src/components/body/pillars/Pair.js
+++ src/components/body/pillars/Pair.js
@@ -7,8 +7,8 @@ class Pair extends Component {
 
     return (
       <div className="pillar-container">
-        <h3 className="text-center">Inclusive By Design</h3>
-        <p>If our mission is to be accessible, we must also be inclusive. Structural barriers and power hierarchies are abundant in society. These inequalities are not only present in the tech industry, but in many ways are exacerbated by it. Cogrammers aims to better our communities by being aware of these power imbalances, and to counter them where possible. We hold all of our members and our partnerships to these same standards. We pledge to listen to and uplift marginalized voices in our community. These voices are our foundation.</p>
+        <h3 className="text-left">Inclusive By Design</h3>
+        <p className="width-50">If our mission is to be accessible, we must also be inclusive. Structural barriers and power hierarchies are abundant in society. These inequalities are not only present in the tech industry, but in many ways are exacerbated by it. Cogrammers aims to better our communities by being aware of these power imbalances, and to counter them where possible. We hold all of our members and our partnerships to these same standards. We pledge to listen to and uplift marginalized voices in our community. These voices are our foundation.</p>
       </div>
     )
   }
