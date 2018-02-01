e8435684ddf950d0b93ea732843c9a5a38fbd42a
SEAjamieD <jamie.degnan@gmail.com>
2018-01-31 22:48:16 -0800

    Add in social icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import FooterBackground from '../../assets/footer-background.svg';
 import TagLogo from '../../assets/footer-tag-logo.svg';
 import SocialPlaceholder from '../../assets/icons/placeholder.svg';
+import MailIcon from '../../assets/icons/mail-icon-gradient.svg';
+import LinkedinIcon from '../../assets/icons/linkedin-icon-gradient.svg';
+import MeetupIcon from '../../assets/icons/meetup-icon-gradient.svg';
+import FacebookIcon from '../../assets/icons/facebook-icon-gradient.svg';
+import TwitterIcon from '../../assets/icons/twitter-icon-gradient.svg';
+
 import './Footer.css';
 
 class Footer extends Component {
@@ -13,11 +19,11 @@ class Footer extends Component {
         <img id="header-logo-tagline" alt="Cogrammers logo and tagline" src={TagLogo}/>
 
         <div className="social-container">
-          <a href="mailto:info@cogrammers.community"><img className="social-logo" src={SocialPlaceholder} alt="Email Icon"/></a>
-          <a href="https://www.linkedin.com/company/cogrammers"><img className="social-logo" src={SocialPlaceholder} alt="LinkedIn Logo icon"/></a>
-          <a href="https://www.meetup.com/cogrammers"><img className="social-logo" src={SocialPlaceholder} alt="Meetup Logo icon"/></a>
-          <a href="https://www.facebook.com/cogrammers"><img className="social-logo" src={SocialPlaceholder} alt="Facebook Logo icon"/></a>
-          <a href="https://www.twitter.com/cogrammers"><img className="social-logo" src={SocialPlaceholder} alt="Twitter Logo icon"/></a>
+          <a href="mailto:info@cogrammers.community"><img className="social-logo" src={MailIcon} alt="Email Icon"/></a>
+          <a href="https://www.linkedin.com/company/cogrammers"><img className="social-logo" src={LinkedinIcon} alt="LinkedIn Logo icon"/></a>
+          <a href="https://www.meetup.com/cogrammers"><img className="social-logo" src={MeetupIcon} alt="Meetup Logo icon"/></a>
+          <a href="https://www.facebook.com/cogrammers"><img className="social-logo" src={FacebookIcon} alt="Facebook Logo icon"/></a>
+          <a href="https://www.twitter.com/cogrammers"><img className="social-logo" src={TwitterIcon} alt="Twitter Logo icon"/></a>
         </div>
       </div>
     )
