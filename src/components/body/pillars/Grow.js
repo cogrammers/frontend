af2da1e1fed081e854a9499c3c487c0be15e9f2f
SEAjamieD <jamie.degnan@gmail.com>
2018-01-31 16:44:33 -0800

    formatted pillars and added placeholder for images, also gave the pillar selector some margin so that it fit better with the new layout

--- src/components/body/pillars/Grow.js
+++ src/components/body/pillars/Grow.js
@@ -7,9 +7,18 @@ class Grow extends Component {
 
     return (
       <div className="pillar-container">
-        <h3 className="text-right">Community Building</h3>
-        <p className="width-50">A core hypothesis of Cogrammers is that the program should directly benefit all those involved, as well as the larger community. Students get an education, alumni grow their network, companies hire better employees, the community gets apps made for them, and more.</p>
-        <p>The community aspect is incredibly important. Cogrammers is more than just a technical handbook. Everything we do is designed to help all of us grow as individuals. By learning empathy, communication, and teamworking skills, we become better people. We become better cohort mates, and better community members.</p>
+
+        <div className="pillar-content-container">
+          <div className="pillar-content margin-right-15">
+            <h3 className="text-left">Community Building</h3>
+            <p className="remove-top-space">A core hypothesis of Cogrammers is that the program should directly benefit all those involved, as well as the larger community. Students get an education, alumni grow their network, companies hire better employees, the community gets apps made for them, and more.</p>
+            <p>The community aspect is incredibly important. Cogrammers is more than just a technical handbook. Everything we do is designed to help all of us grow as individuals. By learning empathy, communication, and teamworking skills, we become better people. We become better cohort mates, and better community members.</p>
+          </div>
+          <div className="image-placeholder pillar-content margin-left-15">
+          placeholder for image
+          </div>
+        </div>
+
       </div>
     )
   }
--- src/components/body/pillars/Learn.js
+++ src/components/body/pillars/Learn.js
@@ -7,9 +7,19 @@ class Learn extends Component {
 
     return (
       <div className="pillar-container">
-        <h3 className="text-center">Open Source</h3>
-        <p>Information is power, and information should be free. Cogrammers is radically transparent. Everything from the curriculum, the discussions, exams, company direction, and business plans should be transparent by default. Current students should feel just as encouraged to change the curriculum as staff or anyone else.</p>
-        <p>Closely tied is the idea that Cogrammers is hackable. To fulfill our mission of being ongoing, we will cherish the idea of trying new ideas, quantifying their impact, and incorporating those concepts. Whether they're by us or a fork. From this iteration, Cogrammers will be world-class.</p>
+        <div className="pillar-content-container">
+
+          <div className="image-placeholder pillar-content margin-right-15">
+           placeholder for image
+          </div>
+
+        <div className="pillar-content margin-left-15 text-right">
+          <h3 className="text-right">Open Source</h3>
+          <p className="remove-top-space">Information is power, and information should be free. Cogrammers is radically transparent. Everything from the curriculum, the discussions, exams, company direction, and business plans should be transparent by default. Current students should feel just as encouraged to change the curriculum as staff or anyone else.</p>
+          <p>Closely tied is the idea that Cogrammers is hackable. To fulfill our mission of being ongoing, we will cherish the idea of trying new ideas, quantifying their impact, and incorporating those concepts. Whether they're by us or a fork. From this iteration, Cogrammers will be world-class.</p>
+        </div>
+
+        </div>
       </div>
     )
   }
